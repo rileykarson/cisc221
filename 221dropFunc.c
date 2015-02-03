c0e9fe66c1051ed448f09ab86df75ae437dab4ad
GrahamSM <graham.sm94@gmail.com>
2015-02-03 00:45:02 -0500

    added 221dropFunc.c

---
+++ 221dropFunc.c
@@ -0,0 +1,20 @@
+int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn){
+	int stopFlag = 0;
+	int index = dropColumn-1;
+	while (stopFlag == 0){
+		if (index + arrayColumn <= arrayRow*arrayColumn){
+			if ((index+arrayColumn) == 1){
+				myArray[index] = 1;
+				stopFlag = 1;				
+			}
+			else{
+				myArray[index] = 1;
+				stopFlag = 1;
+			}			
+		}
+		else{
+			index += dropColumn;
+		}
+	}
+	return index;
+}
