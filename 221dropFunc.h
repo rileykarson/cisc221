d96544abbe258e075b8b2f0c5d29071b99c422a1
GrahamSM <graham.sm94@gmail.com>
2015-02-05 16:30:36 -0500

    Committed 221dropFunc.h header file to repo

---
+++ 221dropFunc.h
@@ -0,0 +1,5 @@
+/*
+	*Piece drop function
+	*Computes resultant position of newly introduced piece into a specific column
+	Return: Position of new piece
+int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn);
