0a6e9804b5108fbd86911e44989b3001bad7e1a5
Riley Karson <rileykarson@users.noreply.github.com>
2015-02-06 00:01:17 -0500

    Update 221dropFunc.h
    Added missing comment block symbol

--- 221dropFunc.h
+++ 221dropFunc.h
@@ -1,5 +1,6 @@
 /*
 	*Piece drop function
 	*Computes resultant position of newly introduced piece into a specific column
-	Return: Position of new piece
+	*Return: Position of new piece
+*/
 int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn);
