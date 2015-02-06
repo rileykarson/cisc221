6a13a221401d0fbd3e4a56b73e0e99fcf48bf1fe
Riley Karson <rileykarson@test.com>
2015-02-06 11:08:51 -0500

    Started making the main function.

--- main.c
+++ main.c
@@ -7,11 +7,18 @@
 	* Return: 0, signalling program success.
 */
 int main(){
-	int *arr;
-	//TODO: Make calls to user input functions
-	int *rows;
-	int *columns;
-	makeArray(arr, rows, columns);
-	gameLoop(arr, rows, columns);
+	int rows;
+	int columns;
+	char symbol;
+	//Resources modified in place.
+	initalPrompt(&rows, &columns, &symbol);
+	int *arr = makeArray(rows, columns);
+	int win = 0;
+	while(win == 0){
+		int dropCol = promptForNextDrop(columns);
+		int pos = pieceDrop(arr, row, column, dropCol);
+		win = win(arr, rows, columns, pos);
+		ascii(arr);
+	}
 	return 0;
 }
