75168c50fcf7e43c491f62536fa4137082bc22f9
Riley Karson <12rk48@pi16.caslab.queensu.ca>
2015-02-10 12:54:58 -0500

    Fixed malloc issue

--- array.c
+++ array.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "array.h"
 
 int* makeArray(int rows, int columns)
 {
 	int arraySize = rows*columns;
-	int baseArray[arraySize];
+	int *baseArray = malloc(sizeof(int) * arraySize);
 	int dummy = 0;
 	for (dummy = 0; dummy < (arraySize-1); dummy++)
 	{
