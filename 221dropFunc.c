8377fa7ab0b5db760b588a232583f24596cbfc37
Graham Stirling-Moffet <12mgs@pi16.caslab.queensu.ca>
2015-02-10 14:26:52 -0500

    main.c

--- 221dropFunc.c
+++ 221dropFunc.c
@@ -4,6 +4,7 @@ int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn, int p
 	int stopFlag = 0;
 	int index = dropColumn-1;
 	while (stopFlag == 0){
+		
 		if  ((index+arrayColumn) >= (arrayRow*arrayColumn)){
 			if ((playCount%2)==0){
 				myArray[index] = 2;
@@ -14,7 +15,10 @@ int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn, int p
 				stopFlag = 1;
 			}
 		}
-		else if ((myArray[index+arrayColumn] == 1) || (myArray[index+arrayColumn] == 2)){
+		else if ((myArray[index+arrayColumn]) != 0){
+			if ((myArray[index]) != 0){
+				return 0;
+			}
 			if ((playCount%2)==0){
 				myArray[index] = 2;
 				stopFlag = 1;
@@ -29,4 +33,6 @@ int pieceDrop(int *myArray, int arrayRow, int arrayColumn, int dropColumn, int p
 	}
 	printf("%d\n", index);
 	return index;
+
 }
+
