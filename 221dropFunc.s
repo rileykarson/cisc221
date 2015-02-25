cfd298d1d76422318d2cd92f16769a35b9598d27
Graham Stirling-Moffet <12mgs@pi16.caslab.queensu.ca>
2015-02-25 18:46:19 -0500

    Committing commented main.s and 221dropFunc.s

--- 221dropFunc.s
+++ 221dropFunc.s
@@ -26,111 +26,111 @@ pieceDrop:
 	add	fp, sp, #4
 	sub	sp, sp, #24
 	str	r0, [fp, #-16]
-	str	r1, [fp, #-20]
-	str	r2, [fp, #-24]
-	str	r3, [fp, #-28]
-	mov	r3, #0
-	str	r3, [fp, #-8]
-	ldr	r3, [fp, #-28]
-	sub	r3, r3, #1
-	str	r3, [fp, #-12]
-	ldr	r3, .L10
-	mov	r0, r3
-	ldr	r1, [fp, #-12]
-	bl	printf
-	b	.L2
+	str	r1, [fp, #-20] 
+	str	r2, [fp, #-24] 
+	str	r3, [fp, #-28] 
+	mov	r3, #0         ///move 0 into r3  or  stopFlag = 0
+	str	r3, [fp, #-8]  ///Store stopFlag at fp-8
+	ldr	r3, [fp, #-28] 
+	sub	r3, r3, #1     ///index = dropCol-1
+	str	r3, [fp, #-12] ///Store index at fp-12
+	ldr	r3, .L10       ///Load first element at .L10 into r3 (.word LC0)
+	mov	r0, r3	       ///Move r3 into r0
+	ldr	r1, [fp, #-12] ///Load index into r1
+	bl	printf	       ///Print index
+	b	.L2	       ///Branch to .L2
 .L9:
-	ldr	r2, [fp, #-12]
-	ldr	r3, [fp, #-24]
-	add	r2, r2, r3
-	ldr	r3, [fp, #-20]
-	ldr	r1, [fp, #-24]
-	mul	r3, r1, r3
-	cmp	r2, r3
-	blt	.L3
+	ldr	r2, [fp, #-12] ///Load index into r2
+	ldr	r3, [fp, #-24] ///Load arrayColumn into r3
+	add	r2, r2, r3     ///index + arrayColumn in r2
+	ldr	r3, [fp, #-20] ///Load arrayRow into r3 
+	ldr	r1, [fp, #-24] ///Load arrayColumn into r1
+	mul	r3, r1, r3     ///arrayRow * arrayColumn into r3
+	cmp	r2, r3	       ///Compare the vales in r2 and r3
+	blt	.L3	       ///If r3<=r2, branch to .L3
 	ldr	r3, [fp, #4]
 	and	r3, r3, #1
-	cmp	r3, #0
-	bne	.L4
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl #2
+	cmp	r3, #0         ///If ((playCount%2)==0)
+	bne	.L4	       ///If not, branch to .L4
+	ldr	r3, [fp, #-12] ///Load index into r3
+	mov	r3, r3, asl #2 
 	ldr	r2, [fp, #-16]
 	add	r3, r2, r3
 	mov	r2, #2
-	str	r2, [r3, #0]
+	str	r2, [r3, #0] ///myArray[index] = 2
 	mov	r3, #1
-	str	r3, [fp, #-8]
-	b	.L2
+	str	r3, [fp, #-8] ///stopFlag = 1
+ 	b	.L2	      ///Branch to L2	
 .L4:
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl #2
+	ldr	r3, [fp, #-12] ///Load index into r3
+	mov	r3, r3, asl #2 
 	ldr	r2, [fp, #-16]
 	add	r3, r2, r3
 	mov	r2, #1
-	str	r2, [r3, #0]
+	str	r2, [r3, #0] ///myArray[index] = 1
 	mov	r3, #1
-	str	r3, [fp, #-8]
-	b	.L2
+	str	r3, [fp, #-8] ///stopFlag = 1
+	b	.L2           ///branch to .L2 
 .L3:
-	ldr	r2, [fp, #-12]
-	ldr	r3, [fp, #-24]
-	add	r3, r2, r3
-	mov	r3, r3, asl #2
-	ldr	r2, [fp, #-16]
-	add	r3, r2, r3
-	ldr	r3, [r3, #0]
-	cmp	r3, #0
-	beq	.L5
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl #2
-	ldr	r2, [fp, #-16]
-	add	r3, r2, r3
-	ldr	r3, [r3, #0]
-	cmp	r3, #0
-	beq	.L6
-	mvn	r3, #0
-	b	.L7
+	ldr	r2, [fp, #-12] ///Load index into r2
+	ldr	r3, [fp, #-24] ///Load  into r3
+	add	r3, r2, r3     /// index + arrayColumn   into r3
+	mov	r3, r3, asl #2 
+	ldr	r2, [fp, #-16] ///Load myArray into r2 
+	add	r3, r2, r3     ///myArray + (index+arrayColumn)asl #2 into r3 
+	ldr	r3, [r3, #0]   ///myArray[index+arrayColumn] into r3
+	cmp	r3, #0         ///if (myArray[index+arrayColumn] != 0)
+	beq	.L5	       ///if it evaluates to 0, branch to .L5
+	ldr	r3, [fp, #-12] ///Load index into r3
+	mov	r3, r3, asl #2 ///index asl #2 into r3 
+	ldr	r2, [fp, #-16] ///myArray into r2
+	add	r3, r2, r3     
+	ldr	r3, [r3, #0]  ///myArray[index]
+	cmp	r3, #0	    ///if (myArray[index] == 0)
+	beq	.L6        ///Branch if equal to .L6
+	mvn	r3, #0     ///???
+	b	.L7	  ///if (myArray[index] != 0)... branch to .L7
 .L6:
 	ldr	r3, [fp, #4]
 	and	r3, r3, #1
-	cmp	r3, #0
-	bne	.L8
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl #2
-	ldr	r2, [fp, #-16]
+	cmp	r3, #0  ///if ((playCount%2) == 0)
+	bne	.L8     ///branch if not equal to .L8	
+	ldr	r3, [fp, #-12] ///Load index into r3
+	mov	r3, r3, asl #2 
+	ldr	r2, [fp, #-16] 
 	add	r3, r2, r3
-	mov	r2, #2
+	mov	r2, #2      ///myArray[index] = 2
 	str	r2, [r3, #0]
-	mov	r3, #1
-	str	r3, [fp, #-8]
-	b	.L2
+	mov	r3, #1    ///stopFlag = 1
+	str	r3, [fp, #-8] ///Store stopFlag at fp-8 
+	b	.L2   	      ///Branch to .L2
 .L8:
-	ldr	r3, [fp, #-12]
-	mov	r3, r3, asl #2
+	ldr	r3, [fp, #-12] ///Load index into r3
+	mov	r3, r3, asl #2 
 	ldr	r2, [fp, #-16]
 	add	r3, r2, r3
-	mov	r2, #1
+	mov	r2, #1     ///myArray[index] = 1
 	str	r2, [r3, #0]
-	mov	r3, #1
-	str	r3, [fp, #-8]
-	b	.L2
+	mov	r3, #1    ///stopFlag = 1
+	str	r3, [fp, #-8] ///Store stopFlag at fp-8
+	b	.L2   	    ///Branch to .L2
 .L5:
 	ldr	r2, [fp, #-12]
 	ldr	r3, [fp, #-24]
-	add	r3, r2, r3
-	str	r3, [fp, #-12]
+	add	r3, r2, r3    ///index += arrayColumn
+	str	r3, [fp, #-12] ///Store index at fp-12
 .L2:
-	ldr	r3, [fp, #-8]
-	cmp	r3, #0
-	beq	.L9
-	ldr	r3, .L10
-	mov	r0, r3
-	ldr	r1, [fp, #-12]
-	bl	printf
-	ldr	r3, [fp, #-12]
+	ldr	r3, [fp, #-8] ///Load stopFlag into r3
+	cmp	r3, #0	      ///Compare stopFlag to 0
+	beq	.L9	      ///If equal, branch to .L9
+	ldr	r3, .L10      ///Load first element stored at .L10 into r3 (.word .LC0)
+	mov	r0, r3	      ///Move r0 into r3
+	ldr	r1, [fp, #-12]///Load index into r1
+	bl	printf	      ///Print index
+	ldr	r3, [fp, #-12]///Load index into r3
 .L7:
-	mov	r0, r3
-	sub	sp, fp, #4
+	mov	r0, r3 ////r3 into r0
+	sub	sp, fp, #4 ///return ??
 	ldmfd	sp!, {fp, pc}
 .L11:
 	.align	2
--- main.s
+++ main.s
@@ -20,76 +20,77 @@
 	.global	main
 	.type	main, %function
 main:
-	@ args = 0, pretend = 0, frame = 32
+	@ args = 0, pretend = 0, frame = 32 ///Variable declaration
 	@ frame_needed = 1, uses_anonymous_args = 0
 	stmfd	sp!, {fp, lr}
-	add	fp, sp, #4
-	sub	sp, sp, #40
-	sub	r0, fp, #28
-	sub	r1, fp, #32
-	sub	r2, fp, #33
-	sub	r3, fp, #34
-	bl	initialPrompt
-	ldr	r2, [fp, #-28]
-	ldr	r3, [fp, #-32]
-	mov	r0, r2
-	mov	r1, r3
-	bl	makeArray
-	str	r0, [fp, #-16]
-	mov	r3, #0
-	str	r3, [fp, #-8]
-	mov	r3, #1
-	str	r3, [fp, #-12]
-	b	.L2
+	add	fp, sp, #4 ///Reset frame pointer
+	sub	sp, sp, #40 ///sp = sp-40
+	sub	r0, fp, #28 ///fp-28 in r0. Or, store rows in r0
+	sub	r1, fp, #32 ///fp-32 in r1. Or, store, columns in r1
+	sub	r2, fp, #33 ///fp-33 in r2. Or, store symbol1 in r2
+	sub	r3, fp, #34 ///fp-34 in r3. Or, store symbol2 in r3
+	bl	initialPrompt ///break w/ link to initial prompt. Initial prompt is called with arguments in r0, r1, r2, r3
+	ldr	r2, [fp, #-28] ///Load fp-28 into r2. Or, load rows into r2
+	ldr	r3, [fp, #-32] /// Load fp-28 into r3. Or, load columns into r3
+	mov	r0, r2 ///Move rows into r0, so that makeArray uses the rows as it's first argument
+	mov	r1, r3 ///Move columns into r1, so that makeArray uses columns as it's second argument
+	bl	makeArray ///Call makeArray
+	str	r0, [fp, #-16] ///Store rows in fp-16
+	mov	r3, #0 ///Move 0 into r3
+	str	r3, [fp, #-8] ///winFlag = 0
+	mov	r3, #1 ///Move 1 into r3
+	str	r3, [fp, #-12] ///playCount = 1
+	b	.L2 ///Branch to .L2
 .L5:
-	ldr	r3, [fp, #-32]
-	mov	r0, r3
-	bl	promptForNextDrop
-	str	r0, [fp, #-20]
-	ldr	r2, [fp, #-28]
+	ldr	r3, [fp, #-32] ///Load fp-32 into r3. Or, load dropCol into r3
+	mov	r0, r3 ///Move dropCol into r0 so that promptForNextDrop uses it as it's first (and only) argument
+	bl	promptForNextDrop ///Call promptForNextDrop
+	str	r0, [fp, #-20] ///Store the modified dropCol in fp-20
+	ldr	r2, [fp, #-28] 
 	ldr	r3, [fp, #-32]
 	ldr	r1, [fp, #-12]
 	str	r1, [sp, #0]
-	ldr	r0, [fp, #-16]
+	ldr	r0, [fp, #-16] 
 	mov	r1, r2
 	mov	r2, r3
-	ldr	r3, [fp, #-20]
-	bl	pieceDrop
-	str	r0, [fp, #-24]
+	ldr	r3, [fp, #-20] ///Allocating appropriate variables arr, rows, columns, dropCol, to appropriate registers for argument passing (r0, r1, r2, r3 respectively. playCount is passed via the stack ([sp, #0])
+	bl	pieceDrop ///int pos = pieceDrop(arr, rows, columns, dropCol, playCount)
+	str	r0, [fp, #-24] ///store pos at fp-24
 	ldr	r3, [fp, #-24]
-	cmp	r3, #0
-	blt	.L3
+	cmp	r3, #0 ///if (pos>=0) 
+	blt	.L3   ///branch to .L3 if less than
 	ldr	r2, [fp, #-28]
 	ldr	r3, [fp, #-32]
 	ldr	r0, [fp, #-16]
 	mov	r1, r2
 	mov	r2, r3
-	ldr	r3, [fp, #-24]
-	bl	win
-	str	r0, [fp, #-8]
-	ldr	r3, [fp, #-12]
-	add	r3, r3, #1
+	ldr	r3, [fp, #-24] ///Allocating appropriate variables arr, rows columns, pos to appropriate registers for argumnet passing (r0, r1, r2, r3 respectively)
+	bl	win ///winFlag = win(arr, rows, columns, pos)
+	str	r0, [fp, #-8] ///Store winFlag at fp-8
+	ldr	r3, [fp, #-12] ///Load playCount
+	add	r3, r3, #1 ///playCount += 1
 	str	r3, [fp, #-12]
-	b	.L4
+	b	.L4 ///branch to .L4
 .L3:
 	ldr	r0, .L6
-	bl	puts
+	bl	puts ///printf("Please enter a not full column\n")
 .L4:
 	ldr	r1, [fp, #-28]
 	ldr	r2, [fp, #-32]
 	ldrb	r3, [fp, #-33]	@ zero_extendqisi2
 	ldrb	r0, [fp, #-34]	@ zero_extendqisi2
 	str	r0, [sp, #0]
-	ldr	r0, [fp, #-16]
-	bl	ascii
+	ldr	r0, [fp, #-16] ///Allocating appropriate variables arry, rows, columns, symbol1 to appropriate registers for argument passing (r0, r1, r2, r3 respectively). symbol2 is passed via the stack ([sp, #0])
+
+	bl	ascii ///ascii(arr, rows, columns, symbol1, symbol2)
 .L2:
-	ldr	r3, [fp, #-8]
-	cmp	r3, #0
-	beq	.L5
+	ldr	r3, [fp, #-8] ///winFlag into r3
+	cmp	r3, #0 ///while (winFlag == 0)
+	beq	.L5 ///if winFlag is 0, branch to .L5
 	mov	r3, #0
 	mov	r0, r3
 	sub	sp, fp, #4
-	ldmfd	sp!, {fp, pc}
+	ldmfd	sp!, {fp, pc}  ///returns 0
 .L7:
 	.align	2
 .L6:
